6ab2fb0f64d769e28f51cbb979f1cbf6de3d84dd
Nathan Winger <winger.nathan.contact@gmail.com>
2018-11-27 19:28:28 +0100

    add toolbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {User} from './interfaces/user';
+import {AuthenticationService} from './services/authentication.service';
+import {Router} from '@angular/router';
+import {MatIconRegistry} from '@angular/material';
+import {DomSanitizer} from '@angular/platform-browser';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'houseSkipper';
+export class AppComponent implements OnInit {
+    title = 'HouseSkipper';
+
+    currentUser: User;
+
+    constructor(private _matIconRegistry: MatIconRegistry, private _domSanitizer: DomSanitizer,
+                private _routerService: Router, private _authService: AuthenticationService) {
+        this._authService.currentUser.subscribe(u => this.currentUser = u);
+    }
+
+    logout() {
+        this._authService.logout();
+        this._routerService.navigate(['/login']);
+    }
+    update() {
+        this._routerService.navigate(['/update']);
+    }
+
+    ngOnInit() {
+        this._matIconRegistry.addSvgIcon('icon-menu', this._domSanitizer.bypassSecurityTrustResourceUrl('/assets/images/logo.svg'));
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
+import {JwtInterceptorService} from './interceptors/jwt-interceptor.service';
+import {MatButtonModule, MatIconModule, MatToolbarModule} from '@angular/material';
 
 @NgModule({
   declarations: [
@@ -10,9 +12,15 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+      MatIconModule,
+      MatToolbarModule,
+      MatButtonModule
   ],
-  providers: [],
+  providers: [
+      {provide: HTTP_INTERCEPTORS, useClass: JwtInterceptorService, multi: true},
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
