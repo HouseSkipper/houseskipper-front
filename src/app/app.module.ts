6306e511f7da27a2a31f7926eed5da302ff1750b
Nathan Winger <winger.nathan.contact@gmail.com>
2018-11-28 22:30:19 +0100

    login theme OK

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
 import {JwtInterceptorService} from './interceptors/jwt-interceptor.service';
-import {MatButtonModule, MatIconModule, MatToolbarModule} from '@angular/material';
+import {MatButtonModule, MatCardModule, MatFormFieldModule, MatIconModule, MatInputModule, MatToolbarModule} from '@angular/material';
 import { LoginComponent } from './login/login.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
 import {APP_ROUTES} from './app.routes';
+import {ReactiveFormsModule} from '@angular/forms';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -15,11 +17,16 @@ import {APP_ROUTES} from './app.routes';
     SignUpComponent
   ],
   imports: [
+      ReactiveFormsModule,
       BrowserModule,
       HttpClientModule,
       MatIconModule,
       MatToolbarModule,
       MatButtonModule,
+      MatCardModule,
+      MatFormFieldModule,
+      MatInputModule,
+      BrowserAnimationsModule,
       APP_ROUTES
   ],
   providers: [
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,69 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {User} from '../interfaces/user';
+import {AuthenticationService} from '../services/authentication.service';
+import {first} from 'rxjs/operators';
+import {Router} from '@angular/router';
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+    selector: 'app-login',
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
 
-  ngOnInit() {
-  }
+    private _errorMsg = '';
+    private _form: FormGroup;
+    private readonly _submit$: EventEmitter<User>;
 
+    get form(): FormGroup {
+        return this._form;
+    }
+
+    @Output('submit')
+    get submit$(): EventEmitter<User> {
+        return this._submit$;
+    }
+
+    /**
+     * Function to emit event to submit form and person
+     */
+    submit(user: User) {
+        this._authService.login(user.email, user.password)
+            .pipe(first())
+            .subscribe(
+                data => {
+                    this._router.navigate(['/']);
+                },
+                error => {
+                    this._errorMsg = error;
+                }
+            )
+        this._submit$.emit(user);
+    }
+
+    constructor(private _authService: AuthenticationService, private _router: Router) {
+        this._submit$ = new EventEmitter<User>();
+        this._form = this._buildForm();
+    }
+
+    private _buildForm(): FormGroup {
+        return new FormGroup({
+            email: new FormControl('', Validators.compose([
+                Validators.required
+            ])),
+            password: new FormControl('', Validators.compose([
+                Validators.required
+            ]))
+        });
+    }
+
+    ngOnInit() {
+        this._authService.logout();
+    }
+
+    get errorMsg(): string {
+        return this._errorMsg;
+    }
 }
