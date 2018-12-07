764207ed9448cbea6dad83f3db71314cc66131a3
Louis <louis.navone7@etu.univ-lorraine.fr>
2018-12-07 21:11:51 +0100

    modif server

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/houseSkipper'));
+app.use(express.static(__dirname + 'dist/houseSkipper'));
 
 app.get('/*', function(req,res) {
-    res.sendFile(path.join(__dirname+'/dist/houseSkipper/index.html'));
+    res.sendFile(path.join(__dirname+'dist/houseSkipper/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
