f7e0cc38bf1563867a21d084517d52c0673bac93
Louis <louis.navone7@etu.univ-lorraine.fr>
2019-01-25 11:46:39 +0100

    test

--- server.js
+++ server.js
@@ -12,6 +12,6 @@ app.get('/*', function(req,res) {
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 8000);
 
 //app.listen(56700);
