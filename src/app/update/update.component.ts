867478a68f85d348ff77d5c7d037f8e65bd8e058
LucasSchnabel <schnabel.lucas@gmail.com>
2019-02-08 14:52:25 +0100

    correction

--- src/app/update/update.component.ts
+++ src/app/update/update.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {User} from '../interfaces/user';
 import {UsersService} from '../services/users.service';
+import {AuthenticationService} from '../services/authentication.service';
 
 @Component({
   selector: 'app-update',
@@ -11,10 +12,10 @@ export class UpdateComponent implements OnInit {
 
   private _model: User;
 
-  constructor(private _userService: UsersService) { }
+  constructor(private _userService: UsersService, private _authService: AuthenticationService) { }
 
   ngOnInit() {
-      this._userService.fetchOne().subscribe((user: User) => this._model = user);
+      this._userService.fetchOne(this._authService.currentUserValue.id).subscribe((user: User) => this._model = user);
   }
 
 }
