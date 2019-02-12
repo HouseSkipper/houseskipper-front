d8fcfe41ec99a6f27b5d007e11a466dd399ec29a
valou <valentin.gerard1010@outlook.fr>
2019-02-12 16:10:22 +0100

    ajout test consentement cookies

---
+++ e2e/src/cookies-consent.e2e-spec.ts
@@ -0,0 +1,26 @@
+import { browser, element, by } from 'protractor';
+
+import { CookiesPage } from './cookies-consent.po';
+
+
+describe('HouseSkipper cookies consent test', () => {
+	let page: CookiesPage;
+
+	beforeEach(() => {
+		page = new CookiesPage();
+	});
+
+
+	it('popup should not be present after clicking the acceptance button', () => {
+		page.navigateTo();
+		browser.waitForAngular();
+
+    expect(page.getCookiesConsent().isDisplayed()).toBe(true);
+
+    page.getAcceptButton().click();
+		browser.waitForAngular();
+
+    expect(page.getCookiesConsent().isPresent()).toBe(false);
+	});
+
+});
