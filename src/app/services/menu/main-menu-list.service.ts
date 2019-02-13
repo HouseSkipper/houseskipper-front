47f660b12d1ac6cc302edc15191fadcd69d596a4
Louis <louis.navone7@etu.univ-lorraine.fr>
2019-02-13 15:20:48 +0100

    Début ajout des logements dans le menu Résidence

--- src/app/services/menu/main-menu-list.service.ts
+++ src/app/services/menu/main-menu-list.service.ts
@@ -1,20 +1,35 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {MenuListService} from './menu-list.service';
+import {HouseService} from '../house.service';
+import {House} from '../../interfaces/house';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class MainMenuListService implements MenuListService {
 
-    constructor() { }
+
+    constructor(private _houseService: HouseService) {
+    }
 
     getFields() {
         const fields = [];
         fields.push({title: 'Tableau de bord', values: []});
         fields.push({title: 'Travaux', values: ['Demande', 'En cours', 'Finalisé']});
-        fields.push({title: 'Résidence', values: []});
+        // fields.push({title: 'Résidence', values: []});
+        this.getHouses(fields);
         fields.push({title: 'Contact', values: []});
         fields.push({title: 'Compte', values: ['Compétences', 'Modifier mes informations']});
         return fields;
+    }
+
+    private getHouses(fields: any) {
+        this._houseService.fecthAllHouse().subscribe((houses: House[]) => {
+            const tmpHouses = ['Ajouter un logement'];
+            for (let i = 0; i < houses.length; i++) {
+                tmpHouses.push(houses[i].houseName);
+            }
+            fields.push({title: 'Résidence', values: tmpHouses});
+        });
     }
 }
