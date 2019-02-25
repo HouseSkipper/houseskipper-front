e64d3ed38e975f68337656907cfaadf17598b21b
Louis <louis.navone7@etu.univ-lorraine.fr>
2019-02-25 10:05:26 +0100

    menu en couleur si selectionné

--- src/app/shared/side-menu/side-menu.component.ts
+++ src/app/shared/side-menu/side-menu.component.ts
@@ -1,11 +1,11 @@
-import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {AfterViewInit, Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 
 @Component({
     selector: 'app-side-menu',
     templateUrl: './side-menu.component.html',
     styleUrls: ['./side-menu.component.css']
 })
-export class SideMenuComponent implements OnInit {
+export class SideMenuComponent implements OnInit, AfterViewInit {
 
     private _fields;
     private _step;
@@ -56,6 +56,16 @@ export class SideMenuComponent implements OnInit {
     }
 
 
+    fieldParent(f: any): boolean {
+        if (this._field === undefined) {
+            return false;
+        } else if (this._field.title === f.value.title) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
     get step() {
         return this._step;
     }
@@ -63,5 +73,9 @@ export class SideMenuComponent implements OnInit {
     @Input()
     set step(value) {
         this._step = value;
+    }
+
+    ngAfterViewInit(): void {
+        this._field = undefined;
     }
 }
