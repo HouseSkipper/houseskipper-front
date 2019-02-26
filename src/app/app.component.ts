da806264ffad8413ee7bf5d15dbbb9f4a0f639de
Louis <louis.navone7@etu.univ-lorraine.fr>
2019-02-26 09:55:46 +0100

    Tick vert ou orange tableau des habitations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -41,6 +41,7 @@ export class AppComponent implements OnInit {
         this._matIconRegistry.addSvgIcon('icon-fr', this._domSanitizer.bypassSecurityTrustResourceUrl('/assets/images/fr.svg'));
         this._matIconRegistry.addSvgIcon('icon-logo-plat', this._domSanitizer.bypassSecurityTrustResourceUrl('/assets/images/Logo-House-Skipper-long.svg'));
         this._matIconRegistry.addSvgIcon('icon-checked', this._domSanitizer.bypassSecurityTrustResourceUrl('/assets/images/checked.svg'));
+        this._matIconRegistry.addSvgIcon('icon-OrangeChecked', this._domSanitizer.bypassSecurityTrustResourceUrl('/assets/images/orangeChecked.svg'));
     }
 
 }
--- src/app/houses/house/house.component.ts
+++ src/app/houses/house/house.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {HouseService} from '../../services/house.service';
 import {House} from '../../interfaces/house';
 import {Router} from '@angular/router';
+import {el} from '@angular/platform-browser/testing/src/browser_util';
 
 @Component({
   selector: 'app-house',
@@ -11,7 +12,7 @@ import {Router} from '@angular/router';
 export class HouseComponent implements OnInit {
 
     private _dataSource: House[];
-    private _displayedColumns = ['House_Name', 'Type', 'Category', 'Lieu', 'NbDocs' , 'Delete'];
+    private _displayedColumns = ['House_Name', 'Type', 'Category', 'Lieu', 'NbDocs' , 'Delete', 'Check'];
 
     constructor(private _houseService: HouseService, private _router: Router) {
     }
@@ -31,6 +32,37 @@ export class HouseComponent implements OnInit {
     delete(id: string) {
         this._houseService.remove(id).subscribe(null, null, () => this.ngOnInit());
     }
+
+    check(house: House): boolean {
+        for (let i = 0 ; i < house.rooms.length; i++) {
+            if (house.rooms[i].space === 0) {
+                return false;
+            } else if (house.rooms[i].typeChauffage === '') {
+                return false;
+            } else if (house.rooms[i].typeChauffage === 'radiateur' && house.rooms[i].nbRadiateur === 0) {
+                return false;
+            } else if (house.rooms[i].volet === 1 && house.rooms[i].nbVolet === 0) {
+                return false;
+            } else if (house.rooms[i].nbVolet !== house.rooms[i].nbFenetre + house.rooms[i].nbPorteFenetre ){
+                return false;
+            }
+        }
+        if (house.exterieur === 1 && (house.surfaceExterieurGauche + house.surfaceExterieurDroit + house.surfaceExterieurAvant
+            + house.surfaceExterieurArriere + house.outsideSpace) === 0) {
+            return false;
+        } else if (house.classeEnergetique === null ) {
+            return false;
+        } else if (house.electricite === 0 && house.eolienne === 0 && house.gaz === 0 && house.panneauxPhoto === 0 ) {
+            return false;
+        } else if (house.constructionYear === 0 || house.surfaceToiture === 0 || house.revetementExterieur === ''
+        || house.revetementToiture === '') {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+
 }
 
 
