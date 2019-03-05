2fc6e909a8130c0dc57ef4f3863307b6fd64229c
Allae Ddine Lemtiri <ddine.allae@gmail.com>
2019-03-05 09:40:04 +0100

    before merge master

--- src/app/form-task/form-task.component.ts
+++ src/app/form-task/form-task.component.ts
@@ -191,6 +191,14 @@ export class FormTaskComponent implements OnInit, OnChanges {
         control.push(this.getCommentaire(date, auteur, etat, commentaire));
     }
 
+    private addCommentaires(auteur: string, etat: string, commentaire: string) {
+        if (this._datePipe.transform(date, 'yyyy-MM-dd') === null) {
+            date = this.now;
+        }
+        const control = <FormArray>this._form.get('commentaires');
+        control.push(this.getCommentaire(date, auteur, etat, commentaire));
+    }
+
     private addTypeSec(nom: string) {
         const control = <FormArray>this._form.get('typeSecondaires');
         control.push(this.getTypeSec(nom));
