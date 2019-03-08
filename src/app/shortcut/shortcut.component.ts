27b1329d647a62f90ee71a8f658fcbd53ee03d64
Louis <louis.navone7@etu.univ-lorraine.fr>
2019-03-08 09:01:04 +0100

    modif interlocuteurs

--- src/app/shortcut/shortcut.component.ts
+++ src/app/shortcut/shortcut.component.ts
@@ -1,23 +1,7 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {Shortcut} from '../interfaces/shortcuts';
+import {INTERLOCUTEUR} from '../shared/shortcut-area/shortcut-area.component';
 
-
-export interface INTERLOCUTEUR {
-  name: string;
-  picture: string;
-  email: string;
-  function: string;
-}
-
-const interlocuteurs: INTERLOCUTEUR[] = [
-  {name: 'Paul Conducteur', picture: '', email: 'paul.conducteur@houseskippers.com', function: 'Conducteur de travaux'},
-  {name: 'Pierre Courtier', picture: '', email: 'pierre.courtier@houseskippers.com', function: 'Courtier en travaux'},
-  {name: 'Albert Conseiller', picture: '', email: 'albert.conseiller@houseskippers.com', function: 'Conseiller-Expert'},
-  {name: 'Denis Banque', picture: '', email: 'denis.banque@houseskippers.com', function: 'Contact banque'},
-  {name: 'Louis Assurance', picture: '', email: 'louis.assurance@houseskippers.com', function: 'Contact assurance'},
-  {name: 'Eric Juriste', picture: '', email: 'eric.juriste@houseskippers.com', function: 'Contact juriste'},
-  {name: 'Loïc Support', picture: '', email: 'loic.support@houseskippers.com', function: 'Agent de support'},
-];
 
 @Component({
   selector: 'app-shortcut',
@@ -25,13 +9,18 @@ const interlocuteurs: INTERLOCUTEUR[] = [
   styleUrls: ['./shortcut.component.css']
 })
 export class ShortcutComponent implements OnInit {
-  private _interlocuteur: INTERLOCUTEUR[];
+  private _interlocuteur: INTERLOCUTEUR;
 
   constructor() {
-    this._interlocuteur = interlocuteurs;
+    this._interlocuteur = {} as INTERLOCUTEUR;
   }
 
-  get interlocuteur(): INTERLOCUTEUR[] {
+  @Input('interlocuteur')
+  set interlocuteur(i: INTERLOCUTEUR) {
+    this._interlocuteur = i;
+  }
+
+  get interlocuteur(): INTERLOCUTEUR {
     return this._interlocuteur;
   }
 
