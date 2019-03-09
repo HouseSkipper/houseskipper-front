072196ee0fe183be81adda6d1dab0f0d4d28c1bb
Simon HAJEK <simonhajek88@gmail.com>
2019-03-09 08:52:41 +0100

    Revert inscription

--- src/app/sign-up/signup-steps.ts
+++ src/app/sign-up/signup-steps.ts
@@ -1,9 +1,8 @@
-
 import { signupStep } from '../interfaces/signupStep';
 
 export const SIGNUP_STEPS: signupStep[] = [
-	{title: 'Identité', values: ['Prénom', 'Nom']},
-	{title: 'Contact', values: ['Email', 'Telephone']},
-	{title: 'Compte', values: ['Motdepasse', 'Role']},
+	{title: 'Identité', values: ['firstname', 'lastname']},
+	{title: 'Contact', values: ['username', 'telephone']},
+	{title: 'Compte', values: ['password', 'role']},
 	{title: 'Validation', values: ['code']}
 ];
